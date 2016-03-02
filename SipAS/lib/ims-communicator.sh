3876252879ce6b78a94442f8ff42b935963de084
venanzir <riccardo.venanzi@unibo.it>
2016-03-02 12:14:30 +0100

    Delete ims-communicator.sh

--- SipAS/lib/ims-communicator.sh
+++
@@ -1,3 +0,0 @@
-
-            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
-        java -classpath ims-communicator.jar:jmf.jar:$CLASSPATH:$JMFHOME -Dlog4j.configuration=sip-communicator.properties net.java.sip.communicator.SipCommunicator
